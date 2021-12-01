c4a86bc274b4af69e1b3e6dd029a06b4df3b0bd5
sumitdpandey <94846497+sumitdpandey@users.noreply.github.com>
2021-12-01 17:32:54 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,13 +1,18 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
   access_key=var.AWS_ACCESS_KEY
   secret_key=var.AWS_SECRET_KEY 
 }
 resource "aws_instance" "web" {
-  ami           = "ami-04902260ca3d33422"
+  ami           = "ami-0108d6a82a783b352"
   instance_type = "t3.micro"
-
+  key_name      = "DevOps_Sumit_key"
+  subnet_id     = "subnet-01db57110f47cf3ed"
   tags = {
-    Name = "unus-HelloWorld"
+    Name = "Sumit-HelloWorld"
   }
+  vpc_security_group_ids      = [
+  "sg-0ee34d73e07a56b80"
+  ]
+  associate_public_ip_address = "true"
 }
